722acb66174a0ecee27b55feb2ac1cebba5b651e
chengzh2008 <chengzh2008@gmail.com>
2015-02-12 17:18:23 -0800

    get get and post request working

--- test/httpServerTest.js
+++ test/httpServerTest.js
@@ -5,28 +5,66 @@ var chai = require('chai'),
     time = require('time'),
     expect = chai.expect;
 
-require('../http_server');
+require('../lib/httpServer');
 
 chai.use(chaihttp);
 
-
 describe('simple post request', function () {
     var date = new time.Date().toString(),
-        post = {
+        post0 = {
             author: "javascript",
-            date: date,
-            message: "This is the post text"
+            message: "This is the first post text"
+        },
+        post1 = {
+            author: "Java",
+            message: "This is the second post text"
         };
 
-    it('responds to a post request', function (done) {
+    it('responds to a first post request', function (done) {
         chai.request('localhost:3000')
             .post('/posts')
-            .send(post)
+            .send(post0)
             .end(function (err, res) {
                 expect(err).to.eql(null);
-                expect(res.body.author).to.eql('javascript');
-                expect(res.body.date).to.eql(date);
+                expect(res.body.a).to.eql('a');
                 done();
             });
     });
+
+    it('should return the first post by get request', function (done) {
+        chai.request('localhost:3000')
+            .get('/posts/0')
+            .end(function (err, res) {
+                expect(err).to.eql(null);
+                expect(res.body).to.deep.eql(post0);
+                done();
+            });
+
+    });
+
+
+    it('responds to a second post request', function (done) {
+        chai.request('localhost:3000')
+            .post('/posts')
+            .send(post1)
+            .end(function (err, res) {
+                expect(err).to.eql(null);
+                expect(res.body.a).to.eql('a');
+                done();
+            });
+    });
+
+    it('should return the second post by get request', function (done) {
+        chai.request('localhost:3000')
+            .get('/posts/1')
+            .end(function (err, res) {
+                expect(err).to.eql(null);
+                expect(res.body).to.deep.eql(post1);
+                done();
+            });
+
+    });
+
+
 });
+
